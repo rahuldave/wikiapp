5bbdad40d11e4db9758b7a4c8c57bed59a87d7b5
Rahul Dave <rahuldave@gmail.com>
2024-05-22 13:56:49 -0400

    Added support for multiple languages Added support for other platforms

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,13 +1,23 @@
 import click.testing
 import pytest
 import requests
+from wikiapp import console
+
 
 @pytest.fixture
 def runner():
     return click.testing.CliRunner()
 
+@pytest.fixture
+def mock_wikipedia_random_page(mocker):
+    return mocker.patch("wikiapp.wikipedia.random_page")
 
-from wikiapp import console
+# def test_main_uses_specified_language(runner, mock_wikipedia_random_page):
+#     # runner.invoke(console.main, ["--language", "es"])
+#     runner.invoke(console.main)
+#     print(">>>", mock_wikipedia_random_page.call_args)
+#     mock_wikipedia_random_page.assert_called_with(language="en")
+
 
 def test_main_prints_title(runner, mock_requests_get):
     result = runner.invoke(console.main)
@@ -19,6 +29,7 @@ def test_main_invokes_requests_get(runner, mock_requests_get):
 
 def test_main_uses_correct_url(runner, mock_requests_get):
     result = runner.invoke(console.main)
+    print("<<<", mock_requests_get.call_args)
     assert mock_requests_get.call_args[0] == ("https://en.wikipedia.org/api/rest_v1/page/random/summary",)
 
 def test_main_fails_on_request_error(runner, mock_requests_get):
