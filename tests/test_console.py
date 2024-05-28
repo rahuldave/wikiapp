1a62e27595bdc870dbf8c7da7a0ad83b74f69851
Rahul Dave <rahuldave@gmail.com>
2024-05-28 12:20:08 -0400

    formatting

--- tests/test_console.py
+++ tests/test_console.py
@@ -1,6 +1,7 @@
 import click.testing
 import pytest
 import requests
+
 from wikiapp import console
 
 
@@ -8,9 +9,11 @@ from wikiapp import console
 def runner():
     return click.testing.CliRunner()
 
+
 @pytest.fixture
 def mock_wikipedia_random_page(mocker):
     return mocker.patch("wikiapp.wikipedia.random_page")
+
 
 # def test_main_uses_specified_language(runner, mock_wikipedia_random_page):
 #     # runner.invoke(console.main, ["--language", "es"])
@@ -23,19 +26,24 @@ def test_main_prints_title(runner, mock_requests_get):
     result = runner.invoke(console.main)
     assert "Lorem Ipsum" in result.output
 
+
 def test_main_invokes_requests_get(runner, mock_requests_get):
-    result = runner.invoke(console.main)
+    runner.invoke(console.main)
     assert mock_requests_get.called
 
+
 def test_main_uses_correct_url(runner, mock_requests_get):
-    result = runner.invoke(console.main)
-    print("<<<", mock_requests_get.call_args)
-    assert mock_requests_get.call_args[0] == ("https://en.wikipedia.org/api/rest_v1/page/random/summary",)
+    runner.invoke(console.main)
+    assert mock_requests_get.call_args[0] == (
+        "https://en.wikipedia.org/api/rest_v1/page/random/summary",
+    )
+
 
 def test_main_fails_on_request_error(runner, mock_requests_get):
     mock_requests_get.side_effect = Exception("Boom")
     result = runner.invoke(console.main)
     assert result.exit_code == 1
+
 
 def test_main_prints_message_on_request_error(runner, mock_requests_get):
     mock_requests_get.side_effect = requests.RequestException
