a5f3749091033cc5955eaf4527b1d3d3c4dc8f84
Rahul Dave <rahuldave@gmail.com>
2024-05-29 16:39:08 -0400

    Added some docs

--- wikiapp/wikipedia.py
+++ wikiapp/wikipedia.py
@@ -3,17 +3,29 @@ from typing import Any
 import click
 import requests
 
+from pydantic import BaseModel, ValidationError
+
 API_URL = "https://{language}.wikipedia.org/api/rest_v1/page/random/summary"
 
 
-def random_page(language: str = "en") -> Any:
+class Page(BaseModel):
+    "A Page Dataclass to represent the retuen value from wikipedia"
+
+    title: str  # the title of the wikipedia page
+    extract: str  # the extract of the wikipedia page
+
+
+def random_page(
+    language: str = "en",  # The language you want to use, as a two character string
+) -> Page:  # Returns a Page Object
     """Get a random page from Wikipedia."""
     try:
         with requests.get(API_URL.format(language=language), timeout=10) as response:
             response.raise_for_status()
             data = response.json()
+            return Page(**data)
         return data
-    except requests.RequestException as error:
+    except (requests.RequestException, ValidationError) as error:
         message = str(error)
         raise click.ClickException(message) from error
 
