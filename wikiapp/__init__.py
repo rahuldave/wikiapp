d533d4ba12019c5a179773d8a44c0d018e7809e4
Rahul Dave <rahuldave@gmail.com>
2024-06-04 02:05:49 -0400

    fixed out of date lock problem

--- wikiapp/__init__.py
+++ wikiapp/__init__.py
@@ -4,6 +4,6 @@ import warnings
 try:
     __version__ = importlib.metadata.version("wikiapp_rd")
 except importlib.metadata.PackageNotFoundError as e:
-    warnings.warn(f"Could not determine version of wikiapp_rd", stacklevel=1)
+    warnings.warn("Could not determine version of wikiapp_rd", stacklevel=1)
     warnings.warn(str(e), stacklevel=1)
     __version__ = "unknown"
